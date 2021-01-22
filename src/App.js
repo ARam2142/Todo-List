dcd6ee2e467907fafd5f7b30ca91ec0fa5fc5876
Aram2142 <andresaaramirez@aol.com>
2021-01-22 11:00:40 -0500

    what the app.js will look like for now

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from "react"
-//import './App.css';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import './App.css';
+import Main from "./page/Main"
 
 function App() {
-  return (
-   <div>
-    <Header />
-    <Footer />
-   </div>
-  );
+  return <Main />
 }
 
 export default App;
