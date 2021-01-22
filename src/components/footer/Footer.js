7b77a0ee5afa0ef61f747a29dfa48d520ea8e8ac
Aram2142 <andresaaramirez@aol.com>
2021-01-21 23:31:47 -0500

    added footer and header. STill need to adjust these

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./footer.css"
+
+class Footer extends Component {
+    render() {
+        return (
+            <nav className="navbar fixed-bottom navbar-dark bg-dark justify-content-center">
+                <span className="navbar-brand footer-font blue align-middle">
+                    Copyright &copy;2020 | Andrés Ramírez
+                </span>
+            </nav>
+        )
+    }
+
+}
+
+export default Footer;
---
+++ src/page/Main.js
@@ -0,0 +1,7 @@
+import React from "react";
+//import Main
+
+function Main() {
+    return <h1>Todo List</h1>
+}
+export default Main;
